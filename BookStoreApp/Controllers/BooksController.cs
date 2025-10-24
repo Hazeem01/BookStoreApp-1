40a0b88573609ae0cdda61f7113de6ceb2cc460c
hazeem <hazeem@HAZEEM-WINDOWS>
2025-10-24 18:26:49 +0000

    Add project files.

---
+++ BookStoreApp/Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDBContext appDBcontext;
+
+        public BooksController(AppDBContext context)
+        {
+            appDBcontext = context;
+        }
+
+        public async Task<IActionResult> Index()
+        { var books = await appDBcontext.Books.ToListAsync();
+            return View(books);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateBook(Models.Book book)
+        {
+            appDBcontext.Add(book);
+            await appDBcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+
+        public IActionResult CreateBook()
+        {
+            return View();
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ BookStoreApp/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStoreApp.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using BookStoreApp.Models;
+    public class AppDBContext: DbContext
+    {
+      public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
